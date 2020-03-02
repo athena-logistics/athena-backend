3185f2d61fc265de41b6ee88c12b3958e7bef326
Jonatan Männchen <jonatan@maennchen.ch>
2020-03-02 21:36:11 +0000

    Set Deployment on Branches

--- apps/athena_cluster/mix.exs
+++ apps/athena_cluster/mix.exs
@@ -4,7 +4,7 @@ defmodule Athena.Cluster.MixProject do
   def project do
     [
       app: :athena_cluster,
-      version: "0.1.0",
+      version: "0.0.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- apps/athena_web/mix.exs
+++ apps/athena_web/mix.exs
@@ -4,7 +4,7 @@ defmodule AthenaWeb.MixProject do
   def project do
     [
       app: :athena_web,
-      version: "0.1.0",
+      version: "0.0.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
