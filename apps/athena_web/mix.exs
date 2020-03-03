3cebeb1a8e144369943cd1f04529698faa63bbd4
Jonatan Männchen <jonatan@maennchen.ch>
2020-03-03 09:02:33 +0000

    Release v0.0.2

--- apps/athena_web/mix.exs
+++ apps/athena_web/mix.exs
@@ -4,7 +4,7 @@ defmodule AthenaWeb.MixProject do
   def project do
     [
       app: :athena_web,
-      version: "0.0.1",
+      version: "0.0.2",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
