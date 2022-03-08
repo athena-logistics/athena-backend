9fbe5d667cc4784d1b1b354cc6498760c4e51078
Jonatan Männchen <jonatan@maennchen.ch>
2022-03-08 21:10:25 +0100

    Make Sure Relevant ItemGroups of Location are Unique

--- test/athena_web/schema/query/node/location_test.exs
+++ test/athena_web/schema/query/node/location_test.exs
@@ -76,20 +76,38 @@ defmodule AthenaWeb.Schema.Query.Node.LocationTest do
     location = location(event, name: "Gallusplatz")
     item_group = item_group(event, name: "Bier")
     unrelated_item_group = item_group(event, name: "Cookies")
-    item = item(item_group, name: "Lager")
+    item_lager = item(item_group, name: "Lager")
+    item_kloesti = item(item_group, name: "Klösti")
     _unrelated_item = item(unrelated_item_group, name: "Chocolate Chip")
 
-    supply =
-      movement(item, amount: 1, source_location_id: nil, destination_location_id: location.id)
+    supply_lager =
+      movement(item_lager,
+        amount: 1,
+        source_location_id: nil,
+        destination_location_id: location.id
+      )
+
+    supply_kloesti =
+      movement(item_kloesti,
+        amount: 1,
+        source_location_id: nil,
+        destination_location_id: location.id
+      )
 
     consumption =
-      movement(item, amount: 1, source_location_id: location.id, destination_location_id: nil)
+      movement(item_lager,
+        amount: 1,
+        source_location_id: location.id,
+        destination_location_id: nil
+      )
 
     event_node_id = global_id!(:event, event.id)
     location_node_id = global_id!(:location, location.id)
-    supply_node_id = global_id!(:supply, supply.id)
+    supply_lager_node_id = global_id!(:supply, supply_lager.id)
+    supply_kloesti_node_id = global_id!(:supply, supply_kloesti.id)
     item_group_node_id = global_id!(:item_group, item_group.id)
-    item_node_id = global_id!(:item, item.id)
+    item_lager_node_id = global_id!(:item, item_lager.id)
+    item_kloesti_node_id = global_id!(:item, item_kloesti.id)
     consumption_node_id = global_id!(:consumption, consumption.id)
 
     assert result = run!(@query, variables: %{"id" => location_node_id})
@@ -103,7 +121,10 @@ defmodule AthenaWeb.Schema.Query.Node.LocationTest do
                  "updatedAt" => _updated_at,
                  "event" => %{"id" => ^event_node_id},
                  "movementsIn" => %{
-                   "edges" => [%{"node" => %{"id" => ^supply_node_id}}]
+                   "edges" => [
+                     %{"node" => %{"id" => ^supply_lager_node_id}},
+                     %{"node" => %{"id" => ^supply_kloesti_node_id}}
+                   ]
                  },
                  "movementsOut" => %{
                    "edges" => [%{"node" => %{"id" => ^consumption_node_id}}]
@@ -112,10 +133,26 @@ defmodule AthenaWeb.Schema.Query.Node.LocationTest do
                    "edges" => [%{"node" => %{"id" => ^item_group_node_id}}]
                  },
                  "items" => %{
-                   "edges" => [%{"node" => %{"id" => ^item_node_id}}]
+                   "edges" => [
+                     %{"node" => %{"id" => ^item_kloesti_node_id}},
+                     %{"node" => %{"id" => ^item_lager_node_id}}
+                   ]
                  },
                  "stock" => %{
                    "edges" => [
+                     %{
+                       "node" => %{
+                         "consumption" => 0,
+                         "movementIn" => 0,
+                         "movementOut" => 0,
+                         "stock" => 1,
+                         "supply" => 1,
+                         "item" => %{"id" => ^item_kloesti_node_id},
+                         "itemGroup" => %{"id" => ^item_group_node_id},
+                         "location" => %{"id" => ^location_node_id},
+                         "status" => "IMPORTANT"
+                       }
+                     },
                      %{
                        "node" => %{
                          "consumption" => 1,
@@ -123,7 +160,7 @@ defmodule AthenaWeb.Schema.Query.Node.LocationTest do
                          "movementOut" => 0,
                          "stock" => 0,
                          "supply" => 1,
-                         "item" => %{"id" => ^item_node_id},
+                         "item" => %{"id" => ^item_lager_node_id},
                          "itemGroup" => %{"id" => ^item_group_node_id},
                          "location" => %{"id" => ^location_node_id},
                          "status" => "IMPORTANT"
