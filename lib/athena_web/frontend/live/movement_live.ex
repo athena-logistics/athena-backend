ea19344fdc3ba327a944585eb113a0d731dab0d7
Jonatan Männchen <jonatan@maennchen.ch>
2022-07-26 14:10:02 +0200

    Improve Moevement Forms

--- lib/athena_web/frontend/live/movement_live.ex
+++ lib/athena_web/frontend/live/movement_live.ex
@@ -7,8 +7,10 @@ defmodule AthenaWeb.Frontend.MovementLive do
   import Ecto.Changeset
 
   alias Athena.Inventory
+  alias Athena.Inventory.Item
   alias Athena.Inventory.Location
   alias Athena.Inventory.Movement
+  alias Athena.Inventory.StockEntry
   alias Ecto.Changeset
   alias Phoenix.PubSub
 
@@ -28,7 +30,13 @@ defmodule AthenaWeb.Frontend.MovementLive do
     {:ok,
      socket
      |> update(event_id)
-     |> assign(changeset: changeset(socket.assigns.live_action, %{id: Ecto.UUID.generate()}))}
+     |> assign(
+       changeset:
+         changeset(socket.assigns.live_action, %{
+           id: Ecto.UUID.generate(),
+           movements: [%{id: Ecto.UUID.generate(), amount: 1}]
+         })
+     )}
   end
 
   @impl Phoenix.LiveView
@@ -84,7 +92,7 @@ defmodule AthenaWeb.Frontend.MovementLive do
       put_embed(
         socket.assigns.changeset,
         :movements,
-        current_movements ++ [%Movement{id: Ecto.UUID.generate()}]
+        current_movements ++ [%Movement{id: Ecto.UUID.generate(), amount: 1}]
       )
 
     {:noreply, assign(socket, changeset: changeset)}
@@ -110,6 +118,31 @@ defmodule AthenaWeb.Frontend.MovementLive do
     {:noreply, assign(socket, changeset: changeset)}
   end
 
+  def handle_event("advance_amount", %{"id" => movement_id, "delta" => delta}, socket) do
+    movements =
+      socket.assigns.changeset
+      |> get_change(
+        :movements,
+        get_field(socket.assigns.changeset, :movements, [])
+      )
+      |> Enum.map(&movement_changeset(&1, %{}))
+      |> Enum.map(fn changeset ->
+        if Changeset.fetch_field!(changeset, :id) == movement_id do
+          Changeset.put_change(
+            changeset,
+            :amount,
+            Changeset.fetch_field!(changeset, :amount) + String.to_integer(delta)
+          )
+        else
+          changeset
+        end
+      end)
+
+    changeset = put_embed(socket.assigns.changeset, :movements, movements)
+
+    {:noreply, assign(socket, changeset: changeset)}
+  end
+
   defp update(socket, event_id) do
     event =
       event_id
@@ -126,11 +159,18 @@ defmodule AthenaWeb.Frontend.MovementLive do
     |> cast(attrs, [:source_location_id, :destination_location_id])
     |> cast_embed(:movements,
       required: true,
-      with: {Movement, :changeset, [%{location_required: false, validate_amount_positive: true}]}
+      with: &movement_changeset/2
     )
     |> validate_action_constraints(action)
     |> patch_root_fields()
   end
+
+  defp movement_changeset(movement, attrs),
+    do:
+      Movement.changeset(movement, attrs, %{
+        location_required: false,
+        validate_amount_positive: true
+      })
 
   defp validate_action_constraints(changeset, action)
 
@@ -156,5 +196,33 @@ defmodule AthenaWeb.Frontend.MovementLive do
       )
 
     put_embed(changeset, :movements, movements)
+  end
+
+  defp get_stock(event, changeset, movement_changeset) do
+    {
+      _get_stock(
+        event,
+        Changeset.fetch_field!(changeset, :source_location_id),
+        Changeset.fetch_field!(movement_changeset, :item_id)
+      ),
+      _get_stock(
+        event,
+        Changeset.fetch_field!(changeset, :destination_location_id),
+        Changeset.fetch_field!(movement_changeset, :item_id)
+      )
+    }
+  end
+
+  defp _get_stock(event, location_id, item_id)
+  defp _get_stock(_event, _location_id, nil), do: nil
+  defp _get_stock(_event, nil, _item_id), do: nil
+
+  defp _get_stock(event, location_id, item_id) do
+    with %Location{stock_entries: stock_entries, id: ^location_id} <-
+           Enum.find(event.locations, &match?(%Location{id: ^location_id}, &1)),
+         %StockEntry{item_id: ^item_id, stock: stock} <-
+           Enum.find(stock_entries, &match?(%StockEntry{item_id: ^item_id}, &1)) do
+      stock
+    end
   end
 end
